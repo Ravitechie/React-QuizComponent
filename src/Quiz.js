c1924802350e5fb571a9885dc48a62409358569e
Ravitechie <42236196+Ravitechie@users.noreply.github.com>
2020-05-08 18:00:00 +0530

    Added QuizQuestion component
    Added QuizQuestion component

--- src/Quiz.js
+++ src/Quiz.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import QuizQuestion from './QuizQuestion.js';
 
 let quizData = require('./quiz_data.json')
 
@@ -11,9 +12,9 @@ class Quiz extends Component{
     }
     render(){
         //console.log(quizData); 
-        return <div>
-            <div className="QuizQuestion">{quizData.quiz_questions[0].instruction_text}
-            </div></div>
+        return <div className ="QuizQuestion">           
+            <QuizQuestion  quiz_question ={quizData.quiz_questions[this.state.quiz_position-1]} />
+            </div>
     }
 }
 export default Quiz;
